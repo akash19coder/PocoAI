a7fced6baf24a1105db789ccc1442b2328ede0a1
iAkash <Akashsah946@gmail.com>
2024-08-24 00:18:05 +0545

    removes footer

--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
-import Footer from "../components/footer";
 import Header from "../components/Header";
 import { Hero } from "../components/Hero";
 import { Pricing } from "../components/Pricing";
-import { HeroHighlight } from "../components/ui/hero-highlight";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,7 +9,6 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between p-2">
         <Hero />
         <Pricing />
-        <Footer />
       </main>
     </>
   );
